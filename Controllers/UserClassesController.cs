1c516f8fafa577efa920fe9319bfbf0a7fafe9a8
Matthew Mitchell <matthewprestonmitchell@gmail.com>
2020-07-19 14:07:22 -0700

    UserClass now view-restricted

--- Controllers/UserClassesController.cs
+++ Controllers/UserClassesController.cs
@@ -22,7 +22,13 @@ namespace WebApplication1.Controllers
         // GET: UserClasses
         public async Task<IActionResult> Index()
         {
-            var webApplication1Context = _context.UserClass.Include(u => u.Class).Include(u => u.IdNavigation);
+            var currentUser = this.User.Identity.Name; //this is current user Email
+            var currentId = _context.UserClass.Include(u => u.Id); //this is user GUID
+            var email = _context.AspNetUsers.Include(u => u.Email); //each records' email property
+            var webApplication1Context = _context.UserClass.Include(u => u.Class).Include(u => u.IdNavigation)
+                .Where(i => i.IdNavigation.Email == currentUser);
+                //.Where(currentUser == email);
+                //.Where(_context.AspNetUsers.);
             return View(await webApplication1Context.ToListAsync());
         }
 
