0a6b694ab7152c28eae795b6fcd88afe22c07a03
Matthew Mitchell <matthewprestonmitchell@gmail.com>
2020-07-30 17:16:48 -0700

    Added API User controller

---
+++ Controllers/UsersAPIController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersAPIController : ControllerBase
+    {
+        private readonly WebApplication1Context _context;
+
+        public UsersAPIController(WebApplication1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UsersAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AspNetUsers>>> GetAspNetUsers()
+        {
+            return await _context.AspNetUsers.ToListAsync();
+        }
+
+        // GET: api/UsersAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AspNetUsers>> GetAspNetUsers(string id)
+        {
+            var aspNetUsers = await _context.AspNetUsers.FindAsync(id);
+
+            if (aspNetUsers == null)
+            {
+                return NotFound();
+            }
+
+            return aspNetUsers;
+        }
+
+        // PUT: api/UsersAPI/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAspNetUsers(string id, AspNetUsers aspNetUsers)
+        {
+            if (id != aspNetUsers.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(aspNetUsers).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AspNetUsersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UsersAPI
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<AspNetUsers>> PostAspNetUsers(AspNetUsers aspNetUsers)
+        {
+            _context.AspNetUsers.Add(aspNetUsers);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (AspNetUsersExists(aspNetUsers.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetAspNetUsers", new { id = aspNetUsers.Id }, aspNetUsers);
+        }
+
+        // DELETE: api/UsersAPI/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<AspNetUsers>> DeleteAspNetUsers(string id)
+        {
+            var aspNetUsers = await _context.AspNetUsers.FindAsync(id);
+            if (aspNetUsers == null)
+            {
+                return NotFound();
+            }
+
+            _context.AspNetUsers.Remove(aspNetUsers);
+            await _context.SaveChangesAsync();
+
+            return aspNetUsers;
+        }
+
+        private bool AspNetUsersExists(string id)
+        {
+            return _context.AspNetUsers.Any(e => e.Id == id);
+        }
+    }
+}
